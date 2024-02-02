7b445a566dc21acadaa6ead878849d19770e0ac2
dhanasra <dhana.spiderlingz@gmail.com>
2024-02-02 22:57:03 +0530

    update: export sheet

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,5 @@
+import '../models/integration.dart';
+
 class Constants {
 
   static List qrEyeStyle = [
@@ -30,6 +32,11 @@ class Constants {
       "#9b59b6", // Purple
       "#e67e22", // Pumpkin
       "#f1c40f", // Yellow
+  ];
+
+  static List<Integration> defaultExports = [
+    const Integration(id: 'csv', name: 'CSV', authLink: '', image: 'https://firebasestorage.googleapis.com/v0/b/bizcard-spiderlingz.appspot.com/o/app_icons%2Fexcel.png?alt=media&token=a54a4f0e-7431-4294-bb62-9274a528ef7e', description: '', group: 'CRM', authType: ''),
+    const Integration(id: 'excel', name: 'Excel', authLink: '', image: 'https://firebasestorage.googleapis.com/v0/b/bizcard-spiderlingz.appspot.com/o/app_icons%2Fcsv.png?alt=media&token=9d342e95-aa6b-488a-b699-7fd19d248720', description: '', group: 'CRM', authType: '')
   ];
 
 }
--- lib/pages/contacts/bottomsheets/export_sheet.dart
+++ lib/pages/contacts/bottomsheets/export_sheet.dart
@@ -1,4 +1,7 @@
+import 'package:bizcard_app/constants/colors_const.dart';
+import 'package:bizcard_app/constants/constants.dart';
 import 'package:bizcard_app/extensions/text_ext.dart';
+import 'package:bizcard_app/models/integration.dart';
 import 'package:bizcard_app/pages/widgets/gap.dart';
 import 'package:bizcard_app/pages/widgets/main_card.dart';
 import 'package:bizcard_app/utils/global.dart';
@@ -10,38 +13,44 @@ class ExportSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      heightFactor: 0.90,
-      child: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-        children: Global.integrations.map((e) => 
-        InkWell(
-          onTap: ()=>onClick(e.id),
-          child: MainCard(
-            color: Colors.white,
-            margin: const EdgeInsets.only(bottom: 16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      'Export to ${e.name}'.bltext(context, color: 'darker'),
-                      const Gap(size: 8),
-                      Text(e.description)
-                    ],
-                  )
+
+    var user = Global.user!;
+    List<Integration> items = Global.integrations.where((element) => element.group=="CRM" && user.integrations.contains(element.id)).toList();
+    items.addAll(Constants.defaultExports);
+
+    return Wrap(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: Column(
+            children: items.map((e) => 
+            InkWell(
+              onTap: ()=>onClick(e.id),
+              child: MainCard(
+                color: Colors.white,
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      child: Image.network(e.image, width: 40),
+                    ),
+                    const Gap(size: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          'Export to ${e.name}'.bltext(context, color: 'darker'),
+                        ],
+                      )
+                    ),
+                    const Icon(Icons.arrow_forward_ios_rounded, color: ColorsConst.greyLight,)
+                  ],
                 ),
-                const Gap(size: 16),
-                SizedBox(
-                  height: 100,
-                  child: Image.network(e.image, width: 70),
-                )
-              ],
-            ),
+              ),
+            )).toList()
           ),
-        )).toList()
-      ),
+        ),
+      ],
     );
   }
 }
