1d2f4825c50c8c7eb015fc699624565e3f730b21
dhanasra <dhana.spiderlingz@gmail.com>
2024-03-08 14:51:16 +0530

    update: linkedin auth

--- lib/constants/urls_const.dart
+++ lib/constants/urls_const.dart
@@ -8,5 +8,5 @@ class UrlsConst {
 
   static String googleAuth = "$apiHost/auth/google";
   static String githubAuth = "$apiHost/auth/github";
-  static String linkedinAuth = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864hy6jn3uyw75&redirect_uri=$apiHost/auth/callback/linkedin&state=foobar&scope=profile%20openid%20email";
+  static String linkedinAuth = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=7782mi1w2h629z&redirect_uri=$apiHost/auth/callback/linkedin&state=foobar&scope=profile%20openid%20email";
 }
